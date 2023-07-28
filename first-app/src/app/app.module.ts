f5ac0b2c8f417878dcd7eb08533d179983b683c0
Rodrigo Inoue <rodrigoinoue1122@gmail.com>
2023-07-28 11:31:58 -0300

    Created pages and components

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CadastroComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
